dbd7598789829db271b907e35287efd28de14d1c
Adrian Krauza <adriankrauza@MacBook-Air-Adrian.local>
2022-02-20 23:10:22 +0100

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
     // parser: '@typescript-eslint/parser',
+    // "extends": "standard",
+    "parser": "babel-eslint",
     extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
     parserOptions: {
         ecmaVersion: 2018,
@@ -12,6 +14,8 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
         'react/no-children-prop': 'off',
         'react/prop-types': 'off',
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-page-custom-font': 'off',
     },
     settings: {
         react: {
