34dc88226521385dc155209626669cbf859ebc4e
Adrian Krauza <adriankrauza@MacBook-Air-Adrian.local>
2022-03-29 20:41:39 +0200

    fix2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-modwule.exports = {
+modwwule.exports = {
     // parser: '@typescript-eslint/parser',
     // "extends": "standard",
     extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
