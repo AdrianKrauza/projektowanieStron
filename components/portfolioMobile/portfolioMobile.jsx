66e40d833eae588f1b853e7dfbd587d20f04533e
Adrian Krauza <adriankrauza@MacBook-Air-Adrian.local>
2022-06-17 15:48:33 +0200

    dsa

--- components/portfolioMobile/portfolioMobile.jsx
+++ components/portfolioMobile/portfolioMobile.jsx
@@ -6,21 +6,25 @@ const PortfolioMobile = ({ children }) => {
     return (
         <div>
             {isMobile ? (
-                <h1
-                    className={
-                        'flex-col text-white text-center text-[8rem] flex h-screen items-center w-screen justify-center'
-                    }
-                >
-                    Strona dostępna <br />
-                    tylko na komputerach <br />
-                    stacjonarnych
-                    <a
-                        href={'https://strony.owocni.pl'}
-                        className={'text-white border-2 rounded-[5rem] mt-[5rem] p-[2rem] text-[6rem]'}
+                <div className={'bg-black'}>
+                    <h1
+                        className={
+                            'flex-col text-white text-center text-[8rem] flex h-screen items-center w-screen justify-center'
+                        }
                     >
-                        Wróć do oferty
-                    </a>
-                </h1>
+                        Strona dostępna
+                        <br />
+                        dla monitorów
+                        <br />
+                        stacjonarnych
+                        <a
+                            href={'https://strony.owocni.pl'}
+                            className={'text-white border-2 rounded-[5rem] mt-[5rem] p-[2rem] text-[6rem]'}
+                        >
+                            Wróć do oferty
+                        </a>
+                    </h1>
+                </div>
             ) : (
                 children
             )}
