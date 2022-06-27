b3cac2aa7512c3b396b43ebbaf4202f42a0b4efa
Adrian Krauza <adriankrauza@MacBook-Air-Adrian.local>
2022-06-27 14:05:26 +0200

    hot-jar

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,17 @@ class MyDocument extends Document {
   setTimeout("setTrackTime(361)", 361000);
   setTimeout("setTrackTime(601)", 601000);
   setTimeout("setTrackTime(1801)", 1801000);
+  <!-- Hotjar Tracking Code for https://strony.owocni.pl -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3039702,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
                         `}
                     </Script>
                 </Head>
